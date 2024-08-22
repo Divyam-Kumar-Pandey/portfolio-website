b6e5ebc768fefa6a7f450bb372749b1abce7b01f
Divyam <divyamkumarp@gmail.com>
2024-08-23 02:09:32 +0530

    headers, nav completed

--- src/components/StairTransition.tsx
+++ src/components/StairTransition.tsx
@@ -2,12 +2,19 @@
 
 import { AnimatePresence, motion } from "framer-motion"
 import { usePathname } from "next/navigation";
+import Stairs from "./Stairs";
 
 const StairTransition = () => {
     const pathname = usePathname();
   return (
     <AnimatePresence mode="wait">
-        <motion.div />
+        <div key={pathname}>
+            <div className="fixed h-screen w-screen top-0 left-0 right-0 pointer-events-none z-40 flex">
+                <Stairs />
+            </div>
+
+            <motion.div className="h-screen w-screen fixed bg-primary top-0 pointer-events-none" initial={{opacity: 1}} animate={{opacity: 0, transition : {delay: 1, duration: 0.4, ease : "easeInOut"}}}/>
+        </div>
     </AnimatePresence>
   )
 }
